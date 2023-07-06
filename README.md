bc7d49d13508be07a717260a2a0f34bdf7aeb2f7
Joe <josephagrane@gmail.com>
2023-07-06 10:02:23 +0100

    updated readme

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ This repository contains the cluster-side scripts used in this [EPSRC Vacation I
 | - | - |
 | next_value.py \<filename\> [-r,-reset] | A general purpose iterator resource which stores an integral value inside of a file, retrieiving and incrementing it upon request. |
 | run_simulation.bash \<num_seconds\> | A bash script used in the simulation running job. This script run the UGRDV software alongside the EUFS simulator and data logger for a specified number of seconds. Upon completion, a health check is conducted and the job exits nonzero if the database is corrupted. |
+
+## Environment Variables
+
+Please ensure the following environment variables are set before running any script in this repository.
+
+| Name | Description |
+| - | - |
+| UGRWS_DIR | The path to the root directory of the UGRDV repository. |
+| SCRIPTS_REPO | The path to the root directory of this repository. |
+| DATA_COLLECTION_REPO | The path to the root directory of the sim_data_collection data logger repository. |
---
+++ scripts/run_simulation.bash
@@ -0,0 +1,69 @@
+#!/bin/bash
+################################################################
+## This script runs a simulation on the cluster. It works by  ##
+## simultaneously starting the EUFS simulator and the UGRDV   ##
+## software, after which the data logger is started. The sim- ##
+## ulation is stopped after a certain amount of time.         ##
+## TODO: add a health check after running a simulation.       ##
+################################################################
+
+if [[ -z "${SCRIPTS_REPO}" ]]; then
+        echo "SCRIPTS_REPO not set."
+        exit 1
+fi
+
+source "${SCRIPTS_REPO}/scripts/utils.bash" || exit 1
+
+run_and_log source_ros
+
+if [[ "${#}" -ne "1" ]]; then
+        die "Usage: run_simulation.bash <simulation duration in seconds>"
+fi
+
+simulation_seconds="${1}"
+
+run_and_log cd "${UGRWS_DIR}/ros2_ws" || die "Unable to CD to UGRWS_DIR."
+
+nest_track=$("${SCRIPTS_REPO}/scripts/next_track.py") || die "Couldn't fetch next track."
+log "next_track: ${next_track}"
+
+log_folder="${HOME}/.run_simulation/${next_track}"
+run_and_log mkdir -p "${log_folder}"
+
+run_and_log ros2 launch ugrdv_backup_main eufs_simulated_perception_comms.launch.py \
+        "track:=${next_track}" \
+        "commandMode:=${acceleration}" \
+        "rviz:=false" \
+        "show_rqt_gui:=false" \
+        "pub_ground_truth:=true" \
+        "launch_group:=no_perception" \
+        &> "${log_folder}/eufs_launcher.log" &
+eufs_pid="${!}"
+
+log "Waiting for nodes to spin up."
+run_and_log sleep 30
+
+timeout_seconds=120
+log "Waiting for EUFS_SIM service."
+run_and_log timeout "${timeout_seconds}" bash -c "while ! ros2 service list | grep ros_can/set_mission &> /dev/null; do sleep 1; done; echo Found Service."
+run_and_log timeout "${timeout_seconds}" ros2 service call /ros_can/set_mission eufs_msgs/SetCanState '{"ami_state" : 14, "as_state": 1}'
+if [[ "${?}" -ne "0" ]]; then
+        die "Unable to call EUFS service."
+fi
+run_and_log ros2 run sim_data_collection data_collector --ros-args -p "database:=${next_track}.db3" \
+        &> "${log_folder}/sim_data_collection.log" &
+data_pid="${?}"
+tail -f "${log_folder}/sim_data_collection.log" &
+log "Waiting for simulation to end."
+run_and_log sleep "${simulation_seconds}"
+log "Time's up, stopping data collection."
+run_and_log timeout "${timeout_seconds}" ros2 service call /sim_data_collection/stop_collection std_srvs/Trigger
+if [[ "${?}" -ne "0" ]]; then
+        die "Unable to cleanly stop the data collection service."
+else
+        log "Waiting fro sim_data_collection to exit..."
+        wait "${data_pid}" || die "sim_data_collection exited with nonzero error code ${?}"
+        log "Finished cleanly. Exiting."
+fi
+
+exit 0
