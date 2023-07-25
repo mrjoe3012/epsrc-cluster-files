a40a970c51924d0bb12fc433017f5787b1186db3
Joe <josephagrane@gmail.com>
2023-07-25 16:55:39 +0100

    tidied up print statements

--- scripts/analyse_data.bash
+++ scripts/analyse_data.bash
@@ -17,13 +17,9 @@ fi
 num_to_process="${1}"
 databases=(${@:2})
 
-echo "num_to_process: ${num_to_process}"
-echo "datasets: ${databases[@]}"
-
 num_dbs=${#databases[@]}
-echo "num_dbs: ${#databases[@]}"
 
 process_number=$("${SCRIPTS_REPO}/scripts/next_value.py" ${HOME}/proc_num.txt)
 db_idx=$(((num_to_process*process_number)+1))
-echo "db_idx: ${db_idx} process_number: ${process_number} databases: ${databases[@]} num_dbs: ${num_dbs}"
+echo "db_idx: ${db_idx} process_number: ${process_number} databases: ${databases[@]:db_idx:num_to_process} num_dbs: ${num_dbs}"
 ros2 run sim_data_collection analysis analyse analysis.json "${databases[@]:db_idx:num_to_process}"
