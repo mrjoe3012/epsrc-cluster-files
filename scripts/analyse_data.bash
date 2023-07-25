7849e3acf8542c566c36ec47d20f5159011d81eb
Joe <josephagrane@gmail.com>
2023-07-25 16:40:31 +0100

    changed script to accept individual databases instead of manually globbing a directory

--- scripts/analyse_data.bash
+++ scripts/analyse_data.bash
@@ -11,21 +11,17 @@ source "${SCRIPTS_REPO}/scripts/utils.bash" || exit 1
 run_and_log source_ros
 
 if [[ "${#}" -ne "2" ]]; then
-    die "Usage: analyse_data.bash <num dbs to process> <dataset directory>"
+    die "Usage: analyse_data.bash <num dbs to process> <dbs>"
 fi
 
 num_to_process="${1}"
-datasets_dir="${2}"
+databases="${@:2}"
 
 echo "num_to_process: ${num_to_process}"
-echo "datasets_dir: ${datasets_dir}"
+echo "datasets: ${databases[@]}"
 
-if ! [[ -d  ${datasets_dir} ]]; then
-    die "${datasets_dir} is not a valid directory."
-fi
-
-databases=("${datasets_dir}"/*)
 num_dbs=${#databases[@]}
+echo "num_dbs: ${#databases[@]}"
 
 process_number=$("${SCRIPTS_REPO}/scripts/next_value.py" ${HOME}/proc_num.txt)
 db_idx=$((((num_dbs/process_number)*num_to_process)+1))
