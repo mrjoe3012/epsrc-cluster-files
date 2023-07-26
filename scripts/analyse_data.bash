9dd6014955fc70b5f0129e989d34dadd888aa5b4
Joe <josephagrane@gmail.com>
2023-07-26 10:24:35 +0100

    added failure condition

--- scripts/analyse_data.bash
+++ scripts/analyse_data.bash
@@ -22,4 +22,4 @@ num_dbs=${#databases[@]}
 process_number=$("${SCRIPTS_REPO}/scripts/next_value.py" ${HOME}/proc_num.txt)
 db_idx=$((num_to_process*process_number))
 echo "db_idx: ${db_idx} process_number: ${process_number} databases: ${databases[@]:db_idx:num_to_process} num_dbs: ${num_dbs}"
-ros2 run sim_data_collection analysis analyse analysis.json "${databases[@]:db_idx:num_to_process}"
+ros2 run sim_data_collection analysis analyse analysis.json "${databases[@]:db_idx:num_to_process}" || die "Unable to run data analysis"
