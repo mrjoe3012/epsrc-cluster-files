8b0c73012b3b74e9257fd5faaab858e275721a3c
Joe <josephagrane@gmail.com>
2023-08-10 10:14:36 +0100

    added kill all ros after sim run in loop

--- scripts/run_simulation_loop.bash
+++ scripts/run_simulation_loop.bash
@@ -12,5 +12,6 @@ for ((i=1;i<num;i++)); do
     while [[ "${code}" -ne "0" ]]; do
         /nfs/cluster-files/scripts/run_simulation.bash "${seconds}" "${perception}"
         code="$?"
+        source /nfs/ugrdv/scripts/KillAllROS.bash
     done
 done
